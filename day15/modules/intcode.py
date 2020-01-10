30d2a2efde352184c0c10907d75d7f36a3766db5
Christofer Tibbelin <christofer.tibbelin@cygate.se>
2020-01-10 10:07:24 +0100

    pep8 fix

--- day15/modules/intcode.py
+++ day15/modules/intcode.py
@@ -23,84 +23,84 @@ class IntCode:
     def add(self):
         ''' function that Add two values together '''
         self.parameter.process(3, 2)
-        v0 = self.parameter.value[0]
-        v1 = self.parameter.value[1]
-        v2 = self.parameter.value[2]
-        self.code_list[v2] = v0 + v1
+        value_0 = self.parameter.value[0]
+        value_1 = self.parameter.value[1]
+        value_2 = self.parameter.value[2]
+        self.code_list[value_2] = value_0 + value_1
         self.pointer.instruction += 4
 
     def multiply(self):
         ''' function that Multiply two values together '''
         self.parameter.process(3, 2)
-        v0 = self.parameter.value[0]
-        v1 = self.parameter.value[1]
-        v2 = self.parameter.value[2]
-        self.code_list[v2] = v0 * v1
+        value_0 = self.parameter.value[0]
+        value_1 = self.parameter.value[1]
+        value_2 = self.parameter.value[2]
+        self.code_list[value_2] = value_0 * value_1
         self.pointer.instruction += 4
 
     def get_input(self, robot: Robot):
         ''' Gets input to the program '''
         self.parameter.process(1, 0)
-        v0 = self.parameter.value[0]
-        self.code_list[v0] = robot.handle_input()
+        value_0 = self.parameter.value[0]
+        self.code_list[value_0] = robot.handle_input()
         self.pointer.instruction += 2
 
     def send_output(self, robot: Robot):
         ''' sends output from the program '''
         self.parameter.process(1)
-        v0 = self.parameter.value[0]
-        robot.handle_output(v0)
+        value_0 = self.parameter.value[0]
+        robot.handle_output(value_0)
         self.pointer.instruction += 2
 
     def jump_if_true(self):
         ''' sets the instruction pointer input values are equal '''
         self.parameter.process(2)
-        v0 = self.parameter.value[0]
-        v1 = self.parameter.value[1]
-        if v0 != 0:
-            self.pointer.instruction = v1
+        value_0 = self.parameter.value[0]
+        value_1 = self.parameter.value[1]
+        if value_0 != 0:
+            self.pointer.instruction = value_1
         else:
             self.pointer.instruction += 3
 
     def jump_if_false(self):
         ''' sets the instruction pointer input values are not equal '''
         self.parameter.process(2)
-        v0 = self.parameter.value[0]
-        v1 = self.parameter.value[1]
-        if v0 == 0:
-            self.pointer.instruction = v1
+        value_0 = self.parameter.value[0]
+        value_1 = self.parameter.value[1]
+        if value_0 == 0:
+            self.pointer.instruction = value_1
         else:
             self.pointer.instruction += 3
 
     def if_less_than(self):
         ''' sets values in code list if they are smaller '''
         self.parameter.process(3, 2)
-        v0 = self.parameter.value[0]
-        v1 = self.parameter.value[1]
-        v2 = self.parameter.value[2]
-        if v0 < v1:
-            self.code_list[v2] = 1
+        value_0 = self.parameter.value[0]
+        value_1 = self.parameter.value[1]
+        value_2 = self.parameter.value[2]
+        if value_0 < value_1:
+            self.code_list[value_2] = 1
         else:
-            self.code_list[v2] = 0
+            self.code_list[value_2] = 0
         self.pointer.instruction += 4
 
     def if_equal(self):
         ''' sets values in code list if they are equal '''
         self.parameter.process(3, 2)
-        v0 = self.parameter.value[0]
-        v1 = self.parameter.value[1]
-        v2 = self.parameter.value[2]
-        if v0 == v1:
-            self.code_list[v2] = 1
+        value_0 = self.parameter.value[0]
+        value_1 = self.parameter.value[1]
+        value_2 = self.parameter.value[2]
+        if value_0 == value_1:
+            self.code_list[value_2] = 1
         else:
-            self.code_list[v2] = 0
+            self.code_list[value_2] = 0
         self.pointer.instruction += 4
 
     def change_relative_pointer(self):
         ''' adds to the relative pointer from parameter '''
         self.parameter.process(1)
-        v0 = self.parameter.value[0]
-        self.pointer.relative += v0
+        value_0 = self.parameter.value[0]
+        self.pointer.relative += value_0
         self.pointer.instruction += 2
 
     def __repr__(self):
